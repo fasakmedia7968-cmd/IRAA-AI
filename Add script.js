fee2240a56aba8b2c6d4d9736bf0105c20ebafb2
fasakmedia7968-cmd <fasakmedia7968@gmail.com>
2026-07-13 15:32:58 +0530

    Added creator menu toggle functionality

--- Add script.js
+++ Add script.js
@@ -1,1 +1,7 @@
 // IRAA AI V1 Scripts
+const creatorBtn = document.querySelector(".creator");
+const creatorMenu = document.querySelector(".creator-menu");
+
+creatorBtn.addEventListener("click", () => {
+    creatorMenu.classList.toggle("show");
+});
