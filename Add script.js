cb2e5893e907dc2241980aaaa73c9a0d8edcb5ae
fasakmedia7968-cmd <fasakmedia7968@gmail.com>
2026-07-15 09:01:44 +0530

    Add AI Tools slider navigation

--- Add script.js
+++ Add script.js
@@ -5,3 +5,34 @@ const creatorMenu = document.querySelector(".creator-menu");
 creatorBtn.addEventListener("click", () => {
     creatorMenu.classList.toggle("show");
 });
+
+// =========================
+// AI Tools Slider
+// =========================
+
+const toolsPage = document.querySelector(".tools-page");
+const leftArrow = document.querySelector(".tools-arrows span:first-child");
+const rightArrow = document.querySelector(".tools-arrows span:last-child");
+
+let currentPage = 0;
+const pageWidth = 610; // 5 cards width
+
+rightArrow.addEventListener("click", () => {
+    currentPage++;
+
+    if(currentPage > 1){
+        currentPage = 0;
+    }
+
+    toolsPage.style.transform = `translateX(-${currentPage * pageWidth}px)`;
+});
+
+leftArrow.addEventListener("click", () => {
+    currentPage--;
+
+    if(currentPage < 0){
+        currentPage = 1;
+    }
+
+    toolsPage.style.transform = `translateX(-${currentPage * pageWidth}px)`;
+});
